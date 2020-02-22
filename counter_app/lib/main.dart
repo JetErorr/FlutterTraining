f4413ca4f80f577d88678b53964fa9e2948f4617
Jeet Ardeshna <jardeshna@gmail.com>
2020-02-22 18:33:08 +0530

    Sample App 2

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.indigo,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -39,6 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _resetCounter() {
+    setState(() => _counter = 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,30 +50,53 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: _incrementCounter,
-              color: Colors.teal,
-              child: Text(
-                "Increment",
-                style: TextStyle(color: Colors.white),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Image.asset('assets/img2.png'),
+                padding: EdgeInsets.all(30.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue.withOpacity(0.25),
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
               ),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display4,
-            ),
-            RaisedButton(
-                onPressed: _decrementCounter,
-                color: Colors.red,
-                child: Text(
-                  "Decrement",
-                  style: TextStyle(color: Colors.white),
-                )),
-          ],
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.display4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: _decrementCounter,
+                    color: Colors.red,
+                    child: Text(
+                      "Decrement",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: _incrementCounter,
+                    color: Colors.green,
+                    child: Text(
+                      "Increment",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _resetCounter,
+        backgroundColor: Colors.black87,
+        tooltip: 'Reset Counter',
+        child: new Icon(Icons.refresh),
       ),
     );
   }
