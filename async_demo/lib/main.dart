25ec38412edb40e5b0e07177e12b4c1bb20f4b48
Jeet Ardeshna <jardeshna@gmail.com>
2020-03-02 09:47:48 +0530

    WIP : Async operations demo

---
+++ async_demo/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() => runApp(TimeFall());
+
+class TimeFall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SliderModule(),
+        ],
+      ),
+    )));
+  }
+}
+
+class SliderModule extends StatefulWidget {
+  @override
+  _SliderModuleState createState() => _SliderModuleState();
+}
+
+class _SliderModuleState extends State<SliderModule> {
+  double _slideVal = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('$_slideVal'),
+        Slider(
+          value: _slideVal,
+          onChanged: (doub) {
+            setState(() {
+              _slideVal = doub;
+            });
+          },
+        ),
+        RaisedButton(
+          onPressed: () {
+            Timer(Duration(seconds: 1), () {
+              if (_slideVal < 1.0) {
+                setState(() {
+                  _slideVal += 0.1;
+                });
+              } else if (_slideVal > 0.0) {
+                setState(() {
+                  _slideVal -= 0.1;
+                });
+              }
+            });
+            // Timer(Duration(seconds: 1), () {
+
+            // });
+          },
+        ),
+      ],
+    );
+  }
+}
